da5cc214f1b995d3f9e850adc3fdac19734456a9
hayato94087 <hayato94087@gmail.com>
2024-06-18 09:06:24 +0900

    Transcriptions API を試す

---
+++ transcriptions-demo03.ts
@@ -0,0 +1,17 @@
+import fs from "fs";
+import OpenAI from "openai";
+
+const openai = new OpenAI();
+
+async function main() {
+  const transcription = await openai.audio.transcriptions.create({
+    file: fs.createReadStream("audio/demo1.mp3"),
+    model: "whisper-1",
+    response_format: "verbose_json", // デフォルトはJSON。
+    temperature: 0.8, // デフォルトは0。1.0に近づくほどランダム性が増す
+    timestamp_granularities: ["word"]
+  });
+
+  console.log(transcription);
+}
+main();
